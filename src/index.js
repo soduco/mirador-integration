ac2c2c962c16cc560d40b047d606c926299a39d9
Julien Perret <julien.perret@gmail.com>
2023-10-16 00:18:43 +0200

    simplifying the configuration and setting the exported module as a library

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import Mirador from 'mirador/dist/es/src/index';
 import { miradorImageToolsPlugin } from 'mirador-image-tools';
-
-const config = {
-  id: 'demo',
-  windows: [{
-    imageToolsEnabled: true,
-    imageToolsOpen: true,
-    manifestId: 'https://purl.stanford.edu/sn904cj3429/iiif/manifest',
-  }],
-  theme: {
-    palette: {
-      primary: {
-        main: '#1967d2',
+import miradorDownloadPlugins from 'mirador-dl-plugin';
+//import downloadDialogPlugin from 'mirador-downloaddialog/es';
+export function createViewer(jsonData) {
+  const config = {
+    id: 'main',
+    windows: jsonData.windows,
+    catalog: jsonData.catalog,
+    window: {
+      sideBarPanel: "annotations",
+      sideBarOpen: true,
+      highlightAllAnnotations: true,
+      workspaceControlPanel: {
+        enabled: false,
       },
     },
-  },
-};
+    theme: {
+      palette: {
+        primary: {
+          main: '#1967d2',
+        },
+      },
+    },
+  };
 
-Mirador.viewer(config, [
-  ...miradorImageToolsPlugin,
-]);
+  Mirador.viewer(config, [
+    ...miradorImageToolsPlugin, miradorDownloadPlugins,// downloadDialogPlugin,
+  ]);
+}
