a45d7c098d71283edb6e14069189d87198ad70f1
Julien Perret <julien.perret@gmail.com>
2023-10-16 00:03:44 +0200

    adding the dl plugin and a config json file for the catalog and the initial windows

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: './dist/',
+  },
+  plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /@blueprintjs\/(core|icons)/, // ignore optional UI framework dependencies
+    }),
+  ],
+  performance: {
+    hints: false, // remove WARNING in asset size limit
   },
 };
